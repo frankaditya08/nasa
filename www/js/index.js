cd8fe112777d93b28603e7d3b061dc68e534a94b
frankaditya08 <48473335+frankaditya08@users.noreply.github.com>
2019-05-03 19:38:01 +0700

    Update index.js

--- www/js/index.js
+++ www/js/index.js
@@ -3,7 +3,7 @@ var app = {
     // Application Constructor
     initialize: function() {
     
-        var url = "https://api.nasa.gov/planetary/apod?api_key=EcJfVO92QGimBGgSqsSLv1340rEmlGjJREJ43eBe";
+        var url = "https://api.nasa.gov/planetary/apod?api_key=ac1ut6AfdFtWO0rH5I9tBX7OtnQ0EIlzcGkZnWbK";
         
         $("#loadImage").click(function(){
             $.ajax({
